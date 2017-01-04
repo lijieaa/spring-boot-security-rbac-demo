f33e13ba3c887117f2ac8278c78d37d94bff3cd6
lijieaa <952054767@qq.com>
2017-01-04 21:25:53 +0800

    数据库设计

---
+++ src/spring-boot-security-rbac-demo.sql
@@ -0,0 +1,63 @@
+/*==============================================================*/
+/* DBMS name:      MySQL 5.0                                    */
+/* Created on:     2017/1/4 21:18:05                            */
+/*==============================================================*/
+
+
+
+drop table if exists authority;
+
+drop table if exists role;
+
+drop table if exists user;
+
+
+
+/*==============================================================*/
+/* Table: authority                                             */
+/*==============================================================*/
+create table authority
+(
+   id                   varchar(32) not null comment '主键，使用UUID',
+   rol_id               varchar(32) comment '主键，使用UUID',
+   create_time          datetime comment '创建时间',
+   update_time          datetime comment '更新时间',
+   authority_name       varchar(100) comment '权限名称',
+   authority_description text comment '权限描述',
+   primary key (id)
+);
+
+alter table authority comment '权限表';
+
+/*==============================================================*/
+/* Table: role                                                  */
+/*==============================================================*/
+create table role
+(
+   id                   varchar(32) not null comment '主键，使用UUID',
+   use_id               varchar(32) comment '主键，使用UUID',
+   create_time          datetime comment '创建时间',
+   update_time          datetime comment '更新时间',
+   role_name            varchar(100) comment '角色名称',
+   role_description     text,
+   primary key (id)
+);
+
+alter table role comment '角色表';
+
+/*==============================================================*/
+/* Table: user                                                  */
+/*==============================================================*/
+create table user
+(
+   id                   varchar(32) not null comment '主键，使用UUID',
+   create_time          datetime comment '创建时间',
+   update_time          datetime comment '更新时间',
+   username             varchar(100) comment '用户名',
+   password             varchar(100) comment '密码',
+   primary key (id)
+);
+
+alter table user comment '用户表';
+
+
