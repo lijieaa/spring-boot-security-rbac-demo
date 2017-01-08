306f26cb1ec4018d07c5b315878c8ee92ee9b736
lijieaa <952054767@qq.com>
2017-01-08 22:04:34 +0800

    @RestController不能表单提交，准备换@Controller

--- src/main/java/com/github/lijieaa/MvcConfig.java
+++ src/main/java/com/github/lijieaa/MvcConfig.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.FormHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -17,6 +18,11 @@ import java.util.List;
 
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        super.configureContentNegotiation(configurer);
+    }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
